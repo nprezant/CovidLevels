45db02f23b544148154043866459be4f8c2c54ab
Noah <noah.prezant@gmail.com>
2022-05-05 12:48:21 -0600

    Initial Commit

---
+++ CovidCommunityLevels/CovidCommunityLevelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidCommunityLevelsApp.swift
+//  CovidCommunityLevels
+//
+//  Created by Noah on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CovidCommunityLevelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
