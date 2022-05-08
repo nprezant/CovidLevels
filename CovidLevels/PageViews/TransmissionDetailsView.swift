10cfda879b5bfb021c9db6e5ae3158430185b972
Noah <noah.prezant@gmail.com>
2022-05-08 17:06:31 -0600

    Remove commented out lines

--- CovidLevels/PageViews/TransmissionDetailsView.swift
+++ CovidLevels/PageViews/TransmissionDetailsView.swift
@@ -12,10 +12,6 @@ struct TransmissionDetailsView: View {
     let trans: TransmissionData
     var body: some View {
         VStack(spacing: 10) {
-//            var newCases: String = "<10"
-//            if let newCasesPer100kLast7Days = trans.newCasesPer100kLast7Days {
-//                newCases = "\(newCasesPer100kLast7Days)"=
-//            }
             let newCases = trans.newCasesPer100kLast7Days != nil ? "\(trans.newCasesPer100kLast7Days ?? -1)" : "<10"
             DetailItemView(name: "New cases per 100k, last 7 days (daily)", value: newCases)
             DetailItemView(name: "Positive tests (NAAT), last 7 days (daily)", value: "\(trans.percentPositiveTestsLast7Days)%")
